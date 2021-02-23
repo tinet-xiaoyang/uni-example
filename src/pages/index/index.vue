c9a4d8a7edc1d1c6969174281348f12012e1a949
XiaoYang <mikexfreeze@gmail.com>
2021-02-23 17:53:53 +0800

    uni-id 注册功能演示

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,50 +1,51 @@
 <template>
 	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
 		<view>
-            <text class="title">{{title}}</text>
-        </view>
+			<navigator url="/pages/calculator/calculator">uniCloud操作演示</navigator>
+		</view>
+		<view>
+			<navigator url="/pages/password/password">用户名和密码</navigator>
+		</view>
+		<!-- <view>
+			<navigator url="/pages/mobile/mobile">手机号和验证码</navigator>
+		</view>
+		<view>
+			<navigator url="/pages/email/email">邮箱与验证码</navigator>
+		</view> -->
+		<!-- #ifdef APP-PLUS || MP-WEIXIN -->
+		<view>
+			<navigator url="/pages/weixin/weixin">微信登录</navigator>
+		</view>
+		<!-- #endif -->
+		<!-- #ifdef MP-ALIPAY -->
+		<view>
+			<navigator url="/pages/alipay/alipay">支付宝登录</navigator>
+		</view>
+		<!-- #endif -->
+		<!-- <view>
+			<navigator url="/pages/util/util">通用方法</navigator>
+		</view>
+		<view>
+			<navigator url="/pages/rbac/rbac">角色权限</navigator>
+		</view> -->
 	</view>
 </template>
 
-<script lang="ts">
-    import Vue from 'vue';
-	export default Vue.extend({
+<script>
+	export default {
 		data() {
 			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
 
+			}
 		},
 		methods: {
 
 		}
-	});
+	}
 </script>
 
 <style>
 	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		padding: 15px;
 	}
 </style>
